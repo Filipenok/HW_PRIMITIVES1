2d7e7f29df6ac7266482a91040425b012bee96d3
Filipenok <tvoidrug.filipenok@gmail.com>
2023-08-05 10:41:19 +0400

    first commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int cost = 3090;
+        int mileCost = 20;
+
+        int bonus = cost / mileCost;
+
+        System.out.println("Начислено " + bonus + " бонусных миль(и)");
+    }
+}
